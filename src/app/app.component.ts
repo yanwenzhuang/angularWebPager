b823020123fca9a0d12370e1cb1014c8dcc9f8ef
Yan <850082313@qq.com>
2019-08-06 15:28:37 +0800

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss',
+              './app.componentfooter.scss',
+              './app.componentmainTop.scss',
+              './app.componentbottom.scss' 
+            ]
+})
+export class AppComponent {
+  title = 'myHomework';
+}
